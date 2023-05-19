c63bd9cc34d2b08076610eac7eff9d3191bc5de7
kerochelo <kerochelo@gmail.com>
2023-05-19 22:17:10 +0900

    :wrench:fix schema for rails 7

--- db/schema.rb
+++ db/schema.rb
@@ -2,29 +2,28 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_10_070746) do
-
+ActiveRecord::Schema[7.0].define(version: 2019_08_10_070746) do
   create_table "blogs", force: :cascade do |t|
     t.string "title", null: false
     t.text "content", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
   end
 
   create_table "comments", force: :cascade do |t|
     t.string "name"
     t.text "content", null: false
     t.integer "blog_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["blog_id"], name: "index_comments_on_blog_id"
   end
 
@@ -32,21 +31,23 @@ ActiveRecord::Schema.define(version: 2019_08_10_070746) do
     t.string "name", null: false
     t.integer "status", default: 0, null: false
     t.date "release_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["name"], name: "index_projects_on_name", unique: true
   end
 
   create_table "tasks", force: :cascade do |t|
     t.string "title", null: false
     t.integer "status", default: 0, null: false
-    t.datetime "deadline"
+    t.datetime "deadline", precision: nil
     t.date "completion_date"
     t.text "description"
     t.integer "project_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
     t.index ["project_id"], name: "index_tasks_on_project_id"
   end
 
+  add_foreign_key "comments", "blogs"
+  add_foreign_key "tasks", "projects"
 end
