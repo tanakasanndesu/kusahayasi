c6aaaface8f4b9e627f808f4213239cd1d43d9f4
tanakasanndesu <pana191029@gmail.com>
2023-11-01 18:35:16 +0900

    課題

--- Gemfile
+++ Gemfile
@@ -44,6 +44,8 @@ gem "dartsass-rails", "~> 0.4.0"
 
 gem 'sorcery', "0.16.3"
 
+gem 'rails-i18n'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,7 @@ class User < ApplicationRecord
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
-  validates :email, uniqueness: true
-  validates :email, presence: true
+  validates :email, uniqueness: true, presence: true
   validates :first_name, presence: true, length: { maximum: 255 }
   validates :last_name, presence: true, length: { maximum: 255 }
 end
