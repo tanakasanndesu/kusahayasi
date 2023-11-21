4f0cf778b2f638242fc0b4e74d6d5398d3ae9bcf
tanakasanndesu <pana191029@gmail.com>
2023-11-21 20:18:24 +0900

    課題

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def page_title(page_title = '')
+    base_title = 'RUNTEQ BOARD APP'
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
