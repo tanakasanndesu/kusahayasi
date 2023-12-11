05519df6a5ddbd1087b55f9d6268a6465a94abac
tanakasanndesu <pana191029@gmail.com>
2023-12-11 21:25:01 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -56,6 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_06_100700) do
     t.datetime "reset_password_email_sent_at"
     t.integer "access_count_to_reset_password_page", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token"
   end
 
   add_foreign_key "boards", "users"
