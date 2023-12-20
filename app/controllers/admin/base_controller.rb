9f67b58bc0bd4a74f36a3039f30c7bd54f2cdeec
tanakasanndesu <pana191029@gmail.com>
2023-12-20 19:02:56 +0900

    修正

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,13 +1,15 @@
 class Admin::BaseController < ApplicationController
-    before_action :check_admin
-    layout 'admin/layouts/application'
-private
-    def not_authenticated
-        redirect_to admin_login_path
-        flash[:danger] = "ログインしてください"
-    end
+  before_action :check_admin
+  layout 'admin/layouts/application'
 
-    def check_admin
-        redirect_to root_path, warning: "権限がありません" unless current_user.admin?
-    end
+  private
+
+  def not_authenticated
+    redirect_to admin_login_path
+    flash[:danger] = 'ログインしてください'
+  end
+
+  def check_admin
+    redirect_to root_path, warning: '権限がありません' unless current_user.admin?
+  end
 end
--- app/controllers/admin/user_sessions_controller.rb
+++ app/controllers/admin/user_sessions_controller.rb
@@ -2,7 +2,7 @@ class Admin::UserSessionsController < Admin::BaseController
   skip_before_action :require_login, only: %i[new create]
   skip_before_action :check_admin, only: %i[new create]
   layout 'admin/layouts/admin_login'
-  
+
   def new; end
 
   def create
